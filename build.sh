eb57154a0a504d1f262bd7da616c928bf257302c
FuncSherl <1229853312@qq.com>
2025-01-03 01:22:59 +0800

    Some fix

--- build.sh
+++ build.sh
@@ -20,8 +20,10 @@ done
 
 # Export to pdf.
 # Ref: https://tomshine.hashnode.dev/markdown-pdf
-sudo apt install pandoc
+sudo apt install pandoc -y
 # Install xetex, wait for long time.
-sudo apt-get install texlive-xetex latex-cjk-all
-pandoc --pdf-engine=xelatex -V CJKmainfont="LXGW WenKai Mono" \
+sudo apt install texlive-xetex latex-cjk-all -y
+
+# To find supported chinese font, run: fc-list :lang=zh 
+pandoc --pdf-engine=xelatex -V CJKmainfont="AR PL KaitiM GB" \
        --toc -N -o ${RESF%.*}.pdf  $RESF
