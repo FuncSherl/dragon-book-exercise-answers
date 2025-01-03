9981c27e9a4bb9dd7d53afa596c4f3c3d8a424a8
FuncSherl <1229853312@qq.com>
2025-01-04 01:25:54 +0800

    Fix some compile error and change table, finish build process

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+# set -x
 
 # Merge md files
 mkdir -p build
@@ -18,7 +18,7 @@ for i in `ls -d ../ch*/*`; do
 
 done
 
-
+echo "Installing pandoc and depends..."
 # Export to pdf.
 # Ref: https://tomshine.hashnode.dev/markdown-pdf
 sudo apt install pandoc -y
@@ -26,6 +26,38 @@ sudo apt install pandoc -y
 sudo apt install texlive-xetex latex-cjk-all -y
 
 # To find supported chinese font, run: fc-list :lang=zh 
-pandoc --pdf-engine=xelatex -V CJKmainfont="AR PL KaitiM GB"  \
-       -V mainfont=""\
+# CJKmainfont: Noto Serif CJK SC  # 设置正文中文字体
+# CJKsansfont: Noto Sans CJK SC   # 设置无衬线中文字体（可选）
+# CJKmonofont: Sarasa Mono SC     # 设置代码块中文字体
+# mainfont: Times New Roman       # 设置正文英文字体
+# sansfont: Arial                 # 设置无衬线英文字体（可选）
+# monofont: Courier New           # 设置代码块英文字体
+# 如果不指定CJKsansfont，当table中出现中文时就会报该文字找不到
+
+# Ubuntu 自带了一些高质量的无衬线字体，可以直接使用：
+# Ubuntu：Ubuntu 的默认字体，现代且清晰。
+# Ubuntu Condensed：Ubuntu 字体的紧缩版本，适合标题或紧凑排版。
+# Liberation Sans：与 Arial 类似的开源字体，兼容性好。
+# DejaVu Sans：支持多种语言，包括中文、日文和韩文（CJK）。
+# 这些字体通常已经预装在 Ubuntu 中。如果没有，可以通过以下命令安装：
+# sudo apt install fonts-ubuntu fonts-liberation ttf-dejavu
+
+# Google 的 Noto 字体家族是一个高质量的开源字体集合，支持几乎所有语言（包括中文、日文、韩文等）。
+# Noto Sans：无衬线字体，设计简洁现代。
+# Noto Sans CJK：专为中、日、韩文设计的无衬线字体。
+# 安装如下：
+# sudo apt install fonts-noto fonts-noto-cjk
+
+echo "Start generating pdf..."
+
+set -x
+pandoc --pdf-engine=xelatex -V CJKmainfont="Noto Serif CJK SC" \
+       -V CJKmonofont="Sarasa Mono SC" \
+       -V CJKsansfont="Noto Sans CJK SC" \
+       -V mainfont="DejaVu Sans" \
+       -V sansfont="DejaVu Sans" \
+       -V monofont="Noto Sans Mono" \
        --toc -o ${RESF%.*}.pdf  $RESF
+set +x
+
+echo "Finish pdf generating: "${RESF%.*}.pdf
